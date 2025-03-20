6c2fbc183e7888dbb23ec981a177b948c683bf7f
kiran1632 <kartikkadalage14@gmail.com>
2025-03-20 13:06:58 +0530

    Form

--- python_practice.py
+++ python_practice.py
@@ -2,7 +2,7 @@
 print("Hello world")
 print("test")"
 """
-
+"""
 print("Tringle:")
 
 print("           /|")
@@ -12,8 +12,14 @@ print("        /   |")
 print("       /    |")
 print("      /     |")
 print("     /______|")
+"""
 
-
+char_name="kiran"
+age_year="19"
+mob_no="85305568465"
+print("My name is "+ char_name +" kadalage.")
+print("Age "+ age_year +" years,")
+print("phone number "+ mob_no +",")
 
 
 
