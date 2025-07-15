75fe124460a40d834fec583a4a955bbd95f18243
kiran1632 <kartikkadalage14@gmail.com>
2025-07-15 17:45:54 +0530

    Calender

--- List_function.py
+++ List_function.py
@@ -1,10 +1,9 @@
-lucky_numbers=[1,2,3,4,5]
-friends=["ram","sham","soham","shital","vikas"]
-friends.extend(lucky_numbers)
+num_1 = [1,2,3,4,5]
+friends = ["rakesh","rahul","rohit","sham","ram"]
+friends.extend(num_1)
 friends.append("pinky")
-friends.insert(2,"rohit")
-friends.remove("rohit")
+#friends.insert("raj")
+friends.remove("rahul")
+#friends.insert("raj")
 friends.pop()
-friends.clear()
 print(friends)
-print()
