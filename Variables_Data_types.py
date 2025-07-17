b3622513e3b02a6453416b29b3e0cb6cfa0f99c4
kiran1632 <kartikkadalage14@gmail.com>
2025-07-17 10:37:57 +0530

    variables_&_datatypes

---
+++ Variables_Data_types.py
@@ -0,0 +1,126 @@
+#A variable is like a container used store data.
+#eg.
+x=10
+name="Kartik"
+'''
+Here
+    'x' is a variable storing the number '10'
+    'name' is variable storing string 'Kartik'
+'''
+
+#Data Types in python:
+
+#1] int: (store whole numbers without decimal)
+age=19  
+
+#2]float: (store Desimal numbers)
+pi=3.14
+
+#3]string: (store words,sentence or characters("---"))
+name="kartik"
+
+#4]list: ordered collection (A list is a collection of items, it can be changed)
+fruits=["banana","apple","mango"]
+
+#5]tuple: Fixed collection (Like a list but cannot be changed after creation)
+colors=("red","green","blue")
+
+#6]dict: Dictionary (key-value pairs) used to store related data in key; value format
+student={"name": "kartik","age":19,"course": "BCA"}
+
+#7]set: Unorderd unique items (stores a collection of unique values. order is not guaranteed)
+numbers={1,2,3,4}
+
+#All together in one example
+
+x=10                                                        #int
+pi=3.14                                                     #float
+name="Kartik"                                               #string
+fruits=["banana","apple","mango"]                           #list
+colors=("red","green","blue")                               #tuple   
+student={"name": "kartik","age":19,"course": "BCA"}         #dict
+numbers={1,2,3,4}                                           #set
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
